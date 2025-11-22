1ac422a62922683dab19488f911190e745858f3a
ivl <9637763+ivl86@users.noreply.github.com>
2025-11-22 17:26:13 +0200

    Update hello_world to return pod hostname

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 """Example of flask main file."""
+import os
 from flask import Flask
 app = Flask(__name__)
 
-
+hostname = os.getenv("HOSTNAME")
 @app.route('/api/hello')
 def hello_world():
     """Returns Hello, EDP!"""
-    return 'Hello, EDP!'
+    return f'Response received from pod: {hostname}'
 
 
 if __name__ == '__main__':
